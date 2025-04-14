4b5600714a28d82fc591d08977b2ff402992482c
Alek Kariniemi <alekkariniemi1@gmail.com>
2025-04-14 12:27:43 -0700

    Update server.ts
    url provided for index.html page

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-// server.ts
+import type { Request, Response } from 'express';
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.listen(3000, () => console.log('Server running at http://localhost:3000'));
