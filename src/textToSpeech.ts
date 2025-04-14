f28ddbe1121aa17811c93a910cff30881703f609
Alek Kariniemi <alekkariniemi1@gmail.com>
2025-04-14 14:54:31 -0700

    Update textToSpeech.ts
    Add method to generate a buffer for mp3

--- src/textToSpeech.ts
+++ src/textToSpeech.ts
@@ -30,4 +30,22 @@ export class SpeechGenerator {
     await fs.promises.writeFile(resolvedPath, buffer);
     console.log(`Speech file written to ${resolvedPath}`);
   }
+
+  async generateSpeechBuffer(
+    input: string,
+    instructions: string
+  ): Promise<Buffer> {
+    const model = 'gpt-4o-mini-tts';
+    const voice = 'coral';
+
+    const mp3 = await this.openai.audio.speech.create({
+      model,
+      voice,
+      input,
+      instructions,
+    });
+
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    return buffer;
+  }
 }
