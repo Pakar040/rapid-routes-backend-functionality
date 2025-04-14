61094bab59c47341d89315dbec1a03c506a886ed
Alek Kariniemi <alekkariniemi1@gmail.com>
2025-04-14 15:03:46 -0700

    Update server.ts
    Add comments

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,17 @@ const port = 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+// index.html
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
+// text-to-speech.html
 app.get('/text-to-speech.html', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'text-to-speech.html'));
 });
 
+// text-to-speech generate mp3
 // @ts-ignore: Bun and TypeScript don't like Express overloads
 app.post('/api/text-to-speech', async (req: Request, res: Response) => {
   const body = req.body as { input: string; instructions: string };
