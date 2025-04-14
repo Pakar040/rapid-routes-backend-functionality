37105479e683d72d6cd7462b9e4df52b3db062c0
Alek Kariniemi <alekkariniemi1@gmail.com>
2025-04-14 15:18:35 -0700

    Update server.ts
    Remove unnecessary get requests

--- src/server.ts
+++ src/server.ts
@@ -11,22 +11,6 @@ const port = 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// ==============================
-//            Pages
-// ==============================
-// index.html
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
-
-// text-to-speech.html
-app.get('/text-to-speech.html', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'text-to-speech.html'));
-});
-
-// ==============================
-//            Methods
-// ==============================
 // text-to-speech generate mp3
 // @ts-ignore: Bun and TypeScript don't like Express overloads
 app.post('/api/text-to-speech', async (req: Request, res: Response) => {
